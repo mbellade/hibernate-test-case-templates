07486a72601e7bacc4bffd2fdd180ca57a2207e1
Marco Belladelli <marcobladel@gmail.com>
2025-09-15 09:59:38 +0200

    reproducer

--- orm/hibernate-orm-7/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-7/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -38,24 +38,61 @@ class JPAUnitTestCase {
 	// Entities are auto-discovered, so just add them anywhere on class-path
 	// Add your tests, using standard JUnit.
 	@Test
-	void hhh123Test() throws Exception {
+	void testSimpleQuery() throws Exception {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+
+//		final var cb = entityManager.getCriteriaBuilder();
+//		CriteriaQuery<Tuple> query = cb.createTupleQuery();
+//		var root = query.from( Ancestor.class );
+//		var dscCRoot = cb.treat( root, DescendantTak.class );
+
+//		query.select( cb.tuple(
+//				root.get( JPAUnitTestCase_.Ancestor_.id ).alias( "id" ), cb.coalesce(
+//						dscCRoot.get( JPAUnitTestCase_.DescendantTak_.subtitle ),
+//						dscCRoot.get( "dProp" )
+//				).alias( "description" )
+//		) ).orderBy( cb.asc( root.get( JPAUnitTestCase_.Ancestor_.id ) ) );
+//
+//		final var resultList = entityManager.createQuery( query ).getResultList();
+//		assert resultList != null;
+
+		entityManager.createQuery(
+				"select a.id, treat(a as DescendantTak).dProp as description " +
+						"from Ancestor a " +
+						"order by a.id",
+				Tuple.class
+		).getResultList();
+
+		entityManager.getTransaction().commit();
+		entityManager.close();
+	}
+
+	@Test
+	void testComplexQuery() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
 
 		final var cb = entityManager.getCriteriaBuilder();
 		CriteriaQuery<Tuple> query = cb.createTupleQuery();
 		var root = query.from( Ancestor.class );
+		var dscARoot = cb.treat( root, DescendantA.class );
 		var dscCRoot = cb.treat( root, DescendantTak.class );
+		var dscDRoot = cb.treat( root, DescendantD.class );
 
 		query.select( cb.tuple(
-				root.get( JPAUnitTestCase_.Ancestor_.id ).alias( "id" ),
-				cb.coalesce(
-						dscCRoot.get( JPAUnitTestCase_.DescendantTak_.subtitle ),
-						dscCRoot.get( JPAUnitTestCase_.DescendantTak_.title )
+				root.get( JPAUnitTestCase_.Ancestor_.id ).alias( "id" ), cb.coalesce(
+						cb.coalesce(
+								cb.coalesce(
+										dscARoot.get( JPAUnitTestCase_.DescendantA_.subtitle ),
+										dscARoot.get( JPAUnitTestCase_.DescendantA_.title )
+								), cb.coalesce(
+										dscCRoot.get( JPAUnitTestCase_.DescendantTak_.subtitle ),
+										dscCRoot.get( JPAUnitTestCase_.DescendantTak_.title )
+								)
+						), dscDRoot.get( JPAUnitTestCase_.DescendantD_.subtitle )
 				).alias( "description" )
-		) ).orderBy(
-				cb.asc( root.get( JPAUnitTestCase_.Ancestor_.id ) )
-		);
+		) ).orderBy( cb.asc( root.get( JPAUnitTestCase_.Ancestor_.id ) ) );
 
 		final var resultList = entityManager.createQuery( query ).getResultList();
 		assert resultList != null;
@@ -64,7 +101,7 @@ class JPAUnitTestCase {
 		entityManager.close();
 	}
 
-	@Entity
+	@Entity(name = "Ancestor")
 	@Table(schema = "public", name = "t_ancestor")
 	@Inheritance(strategy = InheritanceType.JOINED)
 	@DiscriminatorColumn(name = "def_type_id")
@@ -73,7 +110,7 @@ class JPAUnitTestCase {
 		private Integer id;
 	}
 
-	@Entity
+	@Entity(name = "DescendantA")
 	@DiscriminatorValue("A")
 	@Table(schema = "public", name = "t_descendant_a")
 	@PrimaryKeyJoinColumn(name = "id", referencedColumnName = "id")
@@ -82,13 +119,13 @@ class JPAUnitTestCase {
 		private String subtitle;
 	}
 
-	@Entity
+	@Entity(name = "DescendantB")
 	@Table(schema = "public", name = "t_descendant_b")
 	@PrimaryKeyJoinColumn(name = "id", referencedColumnName = "id")
 	static abstract class DescendantB extends Ancestor {
 	}
 
-	@Entity
+	@Entity(name = "DescendantTak")
 	@DiscriminatorValue("C")
 	@Table(schema = "public", name = "t_descendant_c")
 	@PrimaryKeyJoinColumn(name = "id", referencedColumnName = "id")
@@ -97,11 +134,12 @@ class JPAUnitTestCase {
 		private String subtitle;
 	}
 
-	@Entity
+	@Entity(name = "DescendantD")
 	@DiscriminatorValue("D")
 	@Table(schema = "public", name = "t_descendant_d")
 	@PrimaryKeyJoinColumn(name = "id", referencedColumnName = "id")
 	public class DescendantD extends DescendantB {
 		private String subtitle;
+		String dProp;
 	}
 }
