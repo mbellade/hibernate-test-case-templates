1a7a0ef2f1578db5000cd51433e2afbecaa6c66b
Marco Belladelli <marcobladel@gmail.com>
2025-11-03 10:12:17 +0100

    reproducer

--- orm/hibernate-orm-7/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-7/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Persistence;
 
 /**
@@ -31,8 +35,50 @@ class JPAUnitTestCase {
 	void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+
+		entityManager.createQuery(
+				"from Parent p join p.mid.child1 c1 left join p.mid.child2 c2",
+				Parent.class
+		).getResultList();
+
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@Entity(name = "Parent")
+	public static class Parent {
+		@Id
+		@GeneratedValue
+		private Long id;
+
+		@ManyToOne
+		private Mid mid;
+	}
+
+	@Entity(name = "Mid")
+	public static class Mid {
+		@Id
+		@GeneratedValue
+		private Long id;
+
+		@ManyToOne
+		private Child1 child1;
+
+		@ManyToOne
+		private Child2 child2;
+	}
+
+	@Entity(name = "Child1")
+	public static class Child1 {
+		@Id
+		@GeneratedValue
+		private Long id;
+	}
+
+	@Entity(name = "Child2")
+	public static class Child2 {
+		@Id
+		@GeneratedValue
+		private Long id;
 	}
 }
